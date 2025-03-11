5fbe0aaddb1bc08e630a737c87d11f1b6dbfc9b1
Utsav Kumar Singh <127019911+uts8434@users.noreply.github.com>
2025-03-12 00:00:54 +0530

    Update Footer.jsx
    link updated

--- fronted/src/assets/Footer.jsx
+++ fronted/src/assets/Footer.jsx
@@ -1,4 +1,5 @@
 import React from "react";
+import { Link } from "react-router-dom";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import {
   faGithub,
@@ -11,21 +12,21 @@ function Footer() {
     <footer className="bg-gray-900 text-white py-6">
       {/* Social Icons */}
       <div className="flex justify-center gap-6 text-3xl mb-4">
-        <a href="#" className="text-gray-400 hover:text-white transition duration-300">
+        <Link to="https://github.com/uts8434"  target="_blank" className="text-gray-400 hover:text-white transition duration-300">
           <FontAwesomeIcon icon={faGithub} />
-        </a>
-        <a href="#" className="text-[#0077B5] hover:text-[#005582] transition duration-300">
+        </Link>
+        <Link to="https://www.linkedin.com/in/utsavkrsingh8434/" target="_blank" className="text-[#0077B5] hover:text-[#005582] transition duration-300">
           <FontAwesomeIcon icon={faLinkedin} />
-        </a>
-        <a href="#" className="text-gray-400 hover:text-white transition duration-300">
+        </Link>
+        <Link to="#" className="text-gray-400 hover:text-white transition duration-300">
           <FontAwesomeIcon icon={faXTwitter} />
-        </a>
+        </Link>
       </div>
 
       {/* Footer Text */}
       <p className="text-center text-gray-400 text-sm">
         &copy; {new Date().getFullYear()} Created by{" "}
-        <span className="text-white font-semibold">Utsav Kumar Singh</span>
+        <span className="text-red-500  font-semibold">Utsav Kumar Singh</span>
       </p>
     </footer>
   );
